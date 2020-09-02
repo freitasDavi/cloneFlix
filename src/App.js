ed145daedab4668432a45676e32c8f65bc93ac0b
Davi <davi.frrs@outlook.com>
2020-09-02 01:00:03 -0300

    Finished Netflix Landing clone

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './tmdb';
 import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie'
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader ] = useState(false)
 
 
   useEffect(() =>{
@@ -28,8 +30,26 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, []);
+
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
       
       {featuredData && 
         <FeaturedMovie item={featuredData} />
@@ -41,6 +61,19 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> por Davi Freitas <br />
+        Direitos de Imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" /> 
+        </div>
+      }
     </div>
   )
 }
